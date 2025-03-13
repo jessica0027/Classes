c3981ac89b300aa42df664f6213581e413d6a611
jessica0027 <jessicacorrea0027@gmail.com>
2025-03-13 19:49:46 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Exercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDetalhes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDetalhes
+            // 
+            this.lblDetalhes.AutoSize = true;
+            this.lblDetalhes.Location = new System.Drawing.Point(50, 23);
+            this.lblDetalhes.Name = "lblDetalhes";
+            this.lblDetalhes.Size = new System.Drawing.Size(49, 13);
+            this.lblDetalhes.TabIndex = 0;
+            this.lblDetalhes.Text = "Detalhes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDetalhes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDetalhes;
+    }
+}
+
---
+++ Exercicio1/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string Descricao { get; set; }
+
+        public Produto (string nome, decimal preco, string descricao)
+        {
+            Nome = nome;
+            Preco = preco;
+            Descricao = descricao;
+        }
+        public string ExibirDetalhes()
+        {
+
+        return $"Nome: {Nome}\n Preco: {Preco}\n Descricao: {Descricao}" ; 
+        
+        }
+    }
+}
